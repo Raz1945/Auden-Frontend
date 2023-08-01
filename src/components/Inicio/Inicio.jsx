5b25e3bac7b44c8fec64905497b7a26fa720e4db
BrunoCurtti <126702544+BrunoCurtti@users.noreply.github.com>
2023-08-01 12:23:19 -0300

    changes 31-7

--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -1,9 +1,48 @@
+import React, { useState } from 'react';
+import './estilo.css';
+import { NavLink } from 'react-router-dom';
+import Logo from './Logo';
+
 const Inicio = () => {
-  return ( 
-    <>
-    <h1>Auden</h1>
-    </>
-   );
-}
- 
+  const [showLogin, setShowLogin] = useState(false);
+  const [showRegistro, setShowRegistro] = useState(false);
+
+  const handleShowLogin = () => {
+    setShowLogin(true);
+    setShowRegistro(false);
+  };
+
+  const handleShowRegistro = () => {
+    setShowRegistro(true);
+    setShowLogin(false);
+  };
+
+  return (
+    <div className='container'>
+      <div className='sub_container'>
+        {!showLogin && !showRegistro ? (
+          <>
+            <div>
+              <Logo />
+              <p className='second_header'>Música a medida</p>
+            </div>
+
+            <div className='btn_order'>
+              <NavLink to='/register' className='btn btn_orange'>
+                Registrarse Gratis
+              </NavLink>
+              <button className='btn btn_wborders'>Continuar con google</button>
+              <button className='btn btn_wborders'>continuar con apple</button>
+            </div>
+
+            <NavLink to='/login' className='btn_offborder'>
+              Iniciar Sesión
+            </NavLink>
+          </>
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
 export default Inicio;
